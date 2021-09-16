c8dc4675dae7c2092d571302d5aa102121d8c1bd
kosyachniy <polozhev@mail.ru>
2021-09-17 01:52:50 +0300

    PyPi package

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+
+setup(
+    name='tggram',
+    version='0.2',
+    description='The simplest library for Telegram bots',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kosyachniy/tggram',
+    author='Alexey Poloz',
+    author_email='polozhev@mail.ru',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Framework :: AsyncIO',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    keywords='simple, Telegram, bot',
+    packages=find_packages(exclude=('tests',)),
+    python_requires='>=3.7, <4',
+    install_requires=[
+        'aiogram',
+    ],
+    project_urls={
+        'Source': 'https://github.com/kosyachniy/tggram',
+    },
+    license='MIT',
+    include_package_data=False,
+)
---
+++ tggram/__init__.py
@@ -0,0 +1,13 @@
+from .main import send, edit, delete, check_entry, forward
+
+
+__all__ = (
+    '__version__',
+    'send',
+    'edit',
+    'delete',
+    'check_entry',
+    'forward',
+)
+
+__version__ = '0.2'
